4d7b04aff8d29982291af939b87e6da8bc726ca7
jboswell-rgb <jboswell@cub.uca.edu>
2026-09-23 15:11:44 -0500

    Refactor input prompts and output formatting

--- main.cpp
+++ main.cpp
@@ -1,29 +1,33 @@
 #include <iostream>
-#include <iostring>
-
+#include <string>
 #include <iomanip>
 
 int main()
 
 {
-	string food_name = "";
-		char item_code = "";
-		int quantity = 0;
-		double unit_price = 0;
-		char member = "";
-		std::cout << "Enter name of the food item";
-		std::cin >> food_name;
-		std::cout << "Enter name of the item code";
-		std::cin >> item_code;
-		std::cout << "Enter the quantity of the item";
-		std::cin >> quantity;
-		std::cout << "Enter the unit price";
-		std::cin >> unit_price;
-		std::cout << "Are you a registered member?";
-		std::cin >> member;
+	std::string food_name = "";
+	int quantity = 0;
+	double unit_price = 0;
+	char item_code = ' ';
+	char member = ' ';
+	std::cout << "Enter name of the food item:\n";
+	std::cin >> food_name;
+	std::cout << "Enter item code:\n";
+	std::cin >> item_code;
+	std::cout << "Enter the quantity of the item:\n";
+	std::cin >> quantity;
+	std::cout << "Enter the unit price:\n";
+	std::cin >> unit_price;
+	std::cout << "Are you a registered member? (y/n)\n";
+	std::cin >> member;
 
-		std::cout << setprecision(2) << fixed << unit_price;
-		std::cont << left << setw(5) << food_name << item_code << quantity << unit_price << member;
-		std::cout << right << setw(6) << 6.5;
+	std::cout << std::left << std::setw(5) << "\nFood name: " << food_name;
+	std::cout << std::left << std::setw(5) << "\nItem code: " << item_code;
+	std::cout << std::left << std::setw(5) << "\nQuantity: " << quantity;
+	std::cout << std::left << std::setw(5) << "\nUnit Price: " << unit_price;
+	std::cout << std::left << std::setw(5) << "\nMember?: " << member;
+	std::cout << std::setprecision(2) << std::fixed << "\nUnit Price Confirmation: " << unit_price;
+	std::cout << std::setw(15) << "\nFood Name Confirmation: " << food_name << std::endl;
+
 
 }
